92bd6ce0de87899755ccf89b68d6a5a8fb899fe4
sonali gandgalekar <sonaligandgalekar@gmail.com>
2025-10-23 16:18:29 +0530

    save to discount-update

--- electricity_bill.py
+++ electricity_bill.py
@@ -1,4 +1,4 @@
-# Electricity Bill Calculator Program
+# Electricity Bill Calculator Program with Discount Feature
 units = int(input("Enter units consumed: "))
 rate_per_unit = 5
 
@@ -6,3 +6,12 @@ total_bill = units * rate_per_unit
 
 print("Units Consumed:", units)
 print("Total Bill: ₹", total_bill)
+
+if total_bill > 1000:
+    discount = total_bill * 0.10
+    final_bill = total_bill - discount
+    print("Discount Applied: ₹", discount)
+    print("Final Bill: ₹", final_bill)
+else:
+    print("No discount applied.")
+    print("Final Bill: ₹", total_bill)
