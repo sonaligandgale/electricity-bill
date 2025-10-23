a0c8d0f6e60d70feb95dc694970d0e27a1d7b088
sonali gandgalekar <sonaligandgalekar@gmail.com>
2025-10-23 16:15:20 +0530

    save

---
+++ electricity_bill.py
@@ -0,0 +1,8 @@
+# Electricity Bill Calculator Program
+units = int(input("Enter units consumed: "))
+rate_per_unit = 5
+
+total_bill = units * rate_per_unit
+
+print("Units Consumed:", units)
+print("Total Bill: ₹", total_bill)
